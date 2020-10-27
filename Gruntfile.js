e30ee40320a7c03adc6bac5c1ab4548ad792a580
wangding <408542507@qq.com>
2020-10-27 08:36:50 +0800

    chore: unescape html and use cdn

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,6 +30,31 @@ module.exports = function (grunt) {
         }]
       }
     },
+    unescape: {
+      files: {
+        src: ['_book/*.html', '_book/docs/*.html'],
+        dist: 'dist/'
+      }
+    },
+    replace: {
+      html: {
+        src: ['_book/*.html', '_book/docs/*.html'],
+        overwrite: true,
+        replacements: [{
+          from: 'href="../gitbook',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/amtb@gh-pages/gitbook'
+        }, {
+          from: 'href="gitbook',
+          to: 'href="https://cdn.jsdelivr.net/gh/wangding/amtb@gh-pages/gitbook'
+        }, {
+          from: 'src="../gitbook',
+          to: 'src="https://cdn.jsdelivr.net/gh/wangding/amtb@gh-pages/gitbook'
+        }, {
+          from: 'src="gitbook',
+          to: 'src="https://cdn.jsdelivr.net/gh/wangding/amtb@gh-pages/gitbook'
+        }]
+      }
+    },
     copy: {
       html: {
         files: [{
@@ -54,6 +79,8 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-text-replace');
+  grunt.loadNpmTasks('grunt-unescape-html');
 
-  grunt.registerTask('minify', ['htmlmin', 'cssmin', 'uglify', 'copy:html', 'copy:js']);
+  grunt.registerTask('minify', ['unescape', 'copy:html','replace', 'htmlmin', 'cssmin', 'uglify', 'copy:html', 'copy:js']);
 };
